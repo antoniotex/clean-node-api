4dc7a39631916396bad0e2bd7d4b386644765197
antoniotex <antoniotx.dev@gmail.com>
2021-12-09 00:33:29 -0300

    Move a criação do Sut

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,10 +1,14 @@
 const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
+const makeSut = () => {
+  return new LoginRouter()
+}
+
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -17,7 +21,7 @@ describe('Login Router', () => {
 
   test('Should return 400 if no password is provided', () => {
     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com'
@@ -30,7 +34,7 @@ describe('Login Router', () => {
 
   test('Should return 500 if no httpRequest is provided', () => {
     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-    const sut = new LoginRouter()
+    const sut = makeSut()
 
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
@@ -38,9 +42,17 @@ describe('Login Router', () => {
 
   test('Should return 500 if no httpRequest has no body', () => {
     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
+
+//   test('Should call AuthUseCase with correct params', () => {
+//     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
+//     const sut = makeSut()
+//     const httpRequest = {}
+//     const httpResponse = sut.route(httpRequest)
+//     expect(httpResponse.statusCode).toBe(500)
+//   })
 })
