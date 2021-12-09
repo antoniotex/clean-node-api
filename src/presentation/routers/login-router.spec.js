824caec7a10e2aa59d45f7d17482486de2c0dbfb
antoniotex <antoniotx.dev@gmail.com>
2021-12-09 00:48:32 -0300

    Teste integração login e authUseCase

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,13 +2,24 @@ const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
 const makeSut = () => {
-  return new LoginRouter()
+  class AuthUseCase {
+    auth (email, password) {
+      this.email = email
+      this.password = password
+    }
+  }
+  const authUseCase = new AuthUseCase()
+  const sut = new LoginRouter(authUseCase)
+  return {
+    sut,
+    authUseCase
+  }
 }
 
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -21,7 +32,7 @@ describe('Login Router', () => {
 
   test('Should return 400 if no password is provided', () => {
     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com'
@@ -34,7 +45,7 @@ describe('Login Router', () => {
 
   test('Should return 500 if no httpRequest is provided', () => {
     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
@@ -42,17 +53,23 @@ describe('Login Router', () => {
 
   test('Should return 500 if no httpRequest has no body', () => {
     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
 
-//   test('Should call AuthUseCase with correct params', () => {
-//     // sut = system under test. Padrão para dizer que é o objeto que estamos testando
-//     const sut = makeSut()
-//     const httpRequest = {}
-//     const httpResponse = sut.route(httpRequest)
-//     expect(httpResponse.statusCode).toBe(500)
-//   })
+  test('Should call AuthUseCase with correct params', () => {
+    // sut = system under test. Padrão para dizer que é o objeto que estamos testando
+    const { sut, authUseCase } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    sut.route(httpRequest)
+    expect(authUseCase.email).toBe(httpRequest.body.email)
+    expect(authUseCase.password).toBe(httpRequest.body.password)
+  })
 })
