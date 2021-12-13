7d5d130fa4b7a09b6f32e7f2afc843429f89de4e
antoniotex <acm.skt@gmail.com>
2021-12-12 23:34:31 -0300

    LoginRouter retorna 500 se passa EmailValidator incorreto

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -194,4 +194,36 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('should return 500 if no EmailValidator is provided', async () => {
+    // sut = system under test. Padrão para dizer que é o objeto que estamos testando
+    // const { sut } = makeSut()
+    const authUseCaseSpy = makeAuthUseCase()
+    const sut = new LoginRouter(authUseCaseSpy)
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('should return 500 if no EmailValidator has no isValid method', async () => {
+    // sut = system under test. Padrão para dizer que é o objeto que estamos testando
+    // const { sut } = makeSut()
+    const authUseCaseSpy = makeAuthUseCase()
+    const sut = new LoginRouter(authUseCaseSpy, {})
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
