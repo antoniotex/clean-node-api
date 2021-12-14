d1e6568a8356ddb09b95632436fd8e3640f3d951
antoniotex <acm.skt@gmail.com>
2021-12-14 02:00:15 -0300

    Move Encrypter para seu proprio arquivo

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,28 +1,26 @@
 const bcrypt = require('bcrypt')
+const Encrypter = require('./encrypter')
 
-class Encrypter {
-  async compare (value, hash) {
-    const isValid = bcrypt.compare(value, hash)
-    return isValid
-  }
+const makeSut = () => {
+  return new Encrypter()
 }
 
 describe('Encrypter', () => {
   test('Should return true if bcrypt returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   test('Should return false if bcrypt returns false', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
   })
 
   test('Should bcrypt with correct values', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     await sut.compare('any_value', 'hashed_value')
     expect(bcrypt.value).toBe('any_value')
     expect(bcrypt.hash).toBe('hashed_value')
