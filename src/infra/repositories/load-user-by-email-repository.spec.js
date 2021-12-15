e1ce3fa59193db37e4b9513a7eadef592c255529
antoniotex <acm.skt@gmail.com>
2021-12-15 09:40:41 -0300

    LoadUserByEmailRepository lança exceptio se nao passar userModel

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -40,4 +40,10 @@ describe('LoadUserByEmail Repository', () => {
     const user = await sut.load('valid_email@mail.com')
     expect(user.email).toBe('valid_email@mail.com')
   })
+
+  test('Should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any_email@mail.com')
+    expect(promise).rejects.toThrow()
+  })
 })
